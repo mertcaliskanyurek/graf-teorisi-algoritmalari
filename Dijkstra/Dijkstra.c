ca794c65cb8b06cc53befde18951865395c5bf81
Mert Çalışkanyürek <m.caliskanyurek@gmail.com>
2018-04-01 11:31:49 +0300

    Dijkstra.c guncellendi

--- Dijkstra/Dijkstra.c
+++ Dijkstra/Dijkstra.c
@@ -2,21 +2,23 @@
 #include<conio.h>
 #include<stdlib.h>
 #include<limits.h>
+#include<string.h>
 typedef struct Ayrit{
         int maaliyet;
-        char *yol; //or: 1-2
+        char *yol; //ornek: 1-2
         char ziyartEdildMi; //1 true 0 false
 }Ayrit;
 
-int **graph;
+int **graph; //grafi temsil eden komsuluk matrisi
 int boyut;
-Ayrit *ayritlar;
+Ayrit *ayritlar; //Grafin ayrit veri yapisi ile temsili
 
 void hafizaAc();
 char dosykmOku(char *dosyAdi);
 void dijkstra();
 char intToChar(int i);
 void ayritYazdir();
+
 int main()
 {
       if(dosykmOku("matrix.amd")==-1)
@@ -32,14 +34,17 @@ int main()
       return 0;
 }
 
+//komsuluk matrisi icin gerekli bellek alanin acilmasi
 void hafizaAc()
 {
- int i;
- graph=(int**)malloc(boyut*sizeof(int *));
-
- for(i=0; i<boyut ; i++ )
- graph[i]=(int *)malloc(boyut*sizeof(int));
+	int i;
+	graph=(int**)malloc(boyut*sizeof(int *));
+	
+	for(i=0; i<boyut ; i++ )
+	graph[i]=(int *)malloc(boyut*sizeof(int));
 }
+
+//komsuluk matrisinin dosyadan okunmasi
 char dosykmOku(char *dosyAdi)
 {
      FILE *fp = fopen(dosyAdi,"r");
@@ -62,7 +67,7 @@ void dijkstra(int baslngDugm)
       ayritlar = (Ayrit*) malloc(boyut*sizeof(Ayrit));
       int i;
 
-      //ayritlari hazirla
+      //ayritlari hazirla tum maaliyetler sonsuz
       for(i=0;i<boyut;i++)
       {
             ayritlar[i].yol = " ";
@@ -83,23 +88,25 @@ void dijkstra(int baslngDugm)
            ayritlar[geldiDugm].ziyartEdildMi = '1';
            ziyaretEdilen++;
            
-           //dugumun komsularini dolasma
+           //dugumun ziyaret edilmemis komsularini dolas ve maaliyetini
+           //geldigi yeri ve ziyaret edilip edilmedigini guncelle
            for(dgm=0;dgm<boyut;dgm++)
            {     	
             	if(ayritlar[dgm].ziyartEdildMi!='1'
-                               &&graph[geldiDugm][dgm]!=0
-      				       &&graph[geldiDugm][dgm]<ayritlar[dgm].maaliyet)
-                  {
-                        char *c = malloc(3*sizeof(char));
-                        c[0]= intToChar(geldiDugm);
-                        c[1]= '-';
-                        c[2]= intToChar(dgm);
-                        
-          	      	ayritlar[dgm].yol = c;
+                        &&graph[geldiDugm][dgm]!=0
+      				    	&&graph[geldiDugm][dgm]<ayritlar[dgm].maaliyet)
+                {
+                	//yolu not edicek gecici degisken
+                    char *temp = malloc(3*sizeof(char));
+                    temp[0]= intToChar(geldiDugm);
+                    temp[1]= '-';
+                    temp[2]= intToChar(dgm);
+                    
+          	      	ayritlar[dgm].yol = temp;
             		ayritlar[dgm].maaliyet = ayritlar[geldiDugm].maaliyet+graph[geldiDugm][dgm];
             		
             		ayritYazdir();
-      		}
+      			}
             }
             
             //yeni dugum icin algoritma tekrar calisacak
@@ -113,48 +120,53 @@ void dijkstra(int baslngDugm)
       printf("Bitti!\n");
       ayritYazdir();
 }
+//ziyaret edilmemis en kucuk ayritin bulunmasi
 int enKuckBul()
 {
-     int i,enki;
-     int enk=INT_MAX;
-     for(i=0;i<boyut;i++)
-     {
-          if(ayritlar[i].ziyartEdildMi=='0' 
-                     && ayritlar[i].maaliyet<enk)
-          {
-              enk=ayritlar[i].maaliyet;
-              enki=i;
-          }
+    int i,enki;
+    int enk=INT_MAX;
+    for(i=0;i<boyut;i++)
+    {
+        if(ayritlar[i].ziyartEdildMi=='0' 
+            && ayritlar[i].maaliyet<enk)
+        {
+            enk=ayritlar[i].maaliyet;
+            enki=i;
+        }
      }
     // printf("%d",enki);
-     if(enk==INT_MAX)
+    if(enk==INT_MAX)
         return -1;
-     else
-         return enki;
+    else
+        return enki;
 }
+//ayritlarin o anki durumunun yazdirilmasi
 void ayritYazdir()
 {
 	int i;
 	for(i=0;i<boyut;i++)
 	{
-             printf(" %c",intToChar(i));
+        printf(" %c",intToChar(i));
              
-             if(ayritlar[i].ziyartEdildMi=='1')
-               printf("* ");
-             else
-                 printf("  ");
+        if(ayritlar[i].ziyartEdildMi=='1')
+            printf("* ");
+        else
+            printf("  ");
               
-             if(ayritlar[i].maaliyet == INT_MAX)   
-                   printf(" SNZ ");
-             else
-                 printf(" %3d ",ayritlar[i].maaliyet);
+        if(ayritlar[i].maaliyet == INT_MAX)   
+            printf(" SNZ ");
+        else
+            printf(" %3d ",ayritlar[i].maaliyet);
                  
-             printf(" %3s ",ayritlar[i].yol);
-             printf("\n");
+        printf(" %3s ",ayritlar[i].yol);
+        printf("\n");
 	}
-       printf("........\n");
+	
+    printf("........\n");
 	getch();
 }
+//ayritlarin geldikleri yerin not edilebilmesi 
+//icin gerekli tip donusumu
 char intToChar(int i)
 {
 	return i+'0';
